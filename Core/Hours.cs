bd392b2648719e7880166c19a9ce3093a772e2b3
UgryumovaNatalya <ugryumova_1994_2@mail.ru>
2015-11-09 18:39:47 +0600

    1
    12

---
+++ Core/Hours.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+   public class Hours : ISalaryCalculator
+   {
+       private int _hour;
+       private int _hourPerMounth;
+
+       public Hours(int hour, int hourPerMounth)
+       { 
+           _hour = hour;
+           _hourPerMounth = hourPerMounth;
+       }
+       public double Calculate()
+       {
+           return _hour * _hourPerMounth;
+       }
+    }
+}
